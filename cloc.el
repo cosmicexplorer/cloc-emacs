4130840e809029a4189b580899c537e7270a7f9c
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-04-26 07:40:27 -0500

    declare cl-lib dependency

--- cloc.el
+++ cloc.el
@@ -4,6 +4,7 @@
 
 ;;; Author: Danny McClanahan <danieldmcclanahan@gmail.com>
 ;;; Version: 2015.04.26
+;;; Package-Requires: ((cl-lib "1.0"))
 ;;; Package-Version: 0.0.0
 ;;; Keywords: cloc, count, source, code, lines
 ;;; URL: https://github.com/cosmicexplorer/cloc-emacs
